0cc40a4ddd3548f4576aafc71e7baa118739cd6c
Jackson Torregrossa <memjkt@gmail.com>
2026-06-01 15:56:11 -0500

    feat: starting webpage scaffolding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,39 @@
 <script>
-	{
-	}
+
 </script>
 
-<div class="flex flex-row h-screen">
-	<span class="text-3xl font-semibold">Void Projects</span>
+<div class="flex h-screen items-center justify-center">
+    <div class="flex flex-col gap-2">
+        <span class="text-5xl font-semibold text-center">Void Projects</span>
+        <span>Software projects for the next generation.</span>
+    </div>
 </div>
+<div class="border-b-1"></div>
+<div class="flex h-96 p-5">
+    <div class="flex flex-row">
+        <div class="flex flex-col gap-2">
+            <span class="text-3xl font-semibold">Constellation</span>
+            <span>Understand your whole world. Never forget it.</span>
+        </div>
+    </div>
+</div>
+<div class="border-b-1"></div>
+<div class="flex h-96 p-5">
+    <div class="flex flex-row">
+        <div class="flex flex-col gap-2">
+            <span class="text-3xl font-semibold">Protostar</span>
+            <span>Skills for everyone.</span>
+        </div>
+    </div>
+</div>
+<div class="border-b-1"></div>
+<div class="flex h-96 p-5">
+    <div class="flex flex-row">
+        <div class="flex flex-col gap-2">
+            <span class="text-3xl font-semibold">Wormhole</span>
+            <span>Almost mind reading.</span>
+        </div>
+    </div>
+</div>
+<footer class="flex h-10 items-center justify-center"></footer>
 
