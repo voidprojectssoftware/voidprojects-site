b283db9501d3acde50899f61d92f5b86d20705dc
Jackson Torregrossa <memjkt@gmail.com>
2026-06-03 10:54:53 -0500

    feat: installed ShadCN, barebones website done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,20 @@
 <script>
-
+	import github from '$lib/assets/github.svg';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
 </script>
 
 <div class="flex h-screen items-center justify-center">
-    <div class="flex flex-col gap-2">
-        <span class="text-5xl font-semibold text-center">Void Projects</span>
-        <span>Software projects for the next generation.</span>
-    </div>
+	<div class="flex flex-col gap-2">
+		<span class="text-center text-5xl font-semibold">Void Projects</span>
+		<span class="text-center">Software projects for the next decade.</span>
+		<Separator class="my-2" />
+		<div class="flex flex-row justify-center">
+			<Button
+				variant="outline"
+				onclick={() => window.open('https://github.com/voidprojectssoftware')}
+				>Github<img src={github} height={32} width={32} alt="GitHub Logo" /></Button
+			>
+		</div>
+	</div>
 </div>
-<div class="border-b-1"></div>
-<div class="flex h-96 p-5">
-    <div class="flex flex-row">
-        <div class="flex flex-col gap-2">
-            <span class="text-3xl font-semibold">Constellation</span>
-            <span>Understand your whole world. Never forget it.</span>
-        </div>
-    </div>
-</div>
-<div class="border-b-1"></div>
-<div class="flex h-96 p-5">
-    <div class="flex flex-row">
-        <div class="flex flex-col gap-2">
-            <span class="text-3xl font-semibold">Protostar</span>
-            <span>Skills for everyone.</span>
-        </div>
-    </div>
-</div>
-<div class="border-b-1"></div>
-<div class="flex h-96 p-5">
-    <div class="flex flex-row">
-        <div class="flex flex-col gap-2">
-            <span class="text-3xl font-semibold">Wormhole</span>
-            <span>Almost mind reading.</span>
-        </div>
-    </div>
-</div>
-<footer class="flex h-10 items-center justify-center"></footer>
-
