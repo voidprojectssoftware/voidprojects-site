2a0290c012ff1849090fe86fffc5da12bd4e9855
Jackson Torregrossa <memjkt@gmail.com>
2026-06-03 10:57:19 -0500

    fix: removed ugly separator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,12 @@
 <script>
 	import github from '$lib/assets/github.svg';
 	import { Button } from '$lib/components/ui/button/index.js';
-	import { Separator } from '$lib/components/ui/separator/index.js';
 </script>
 
 <div class="flex h-screen items-center justify-center">
 	<div class="flex flex-col gap-2">
 		<span class="text-center text-5xl font-semibold">Void Projects</span>
-		<span class="text-center">Software projects for the next decade.</span>
-		<Separator class="my-2" />
+		<span class="text-center">Software for the next decade.</span>
 		<div class="flex flex-row justify-center">
 			<Button
 				variant="outline"
